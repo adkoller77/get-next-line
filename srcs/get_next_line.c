3057c89eafea2fe7143f753b43b837ffa43f643b
Adam Najja <adnajja@fu-r1-p7.s19.be>
2025-06-12 00:11:25 +0200

    end extract line

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adnajja <adnajja@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 19:01:56 by adnajja           #+#    #+#             */
-/*   Updated: 2025/06/09 23:18:47 by adnajja          ###   ########.fr       */
+/*   Updated: 2025/06/11 20:02:52 by adnajja          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,27 @@
 char *extract_line(char **str)
 {
     int i;
+	int j;
     char *line;
     
-    i = 0;
-    while(*str[i] != '\n')
-    {
-        i++;
-    }
-	if(*str[i] == '\n')
+	i = 0;
+	if(*str == NULL)
+		return(NULL);
+	while((*str)[i] != '\n' && (*str)[i] != '\0')
 	{
 		i++;
 	}
+	if ((*str)[i] == '\n')
+		i++;
 	line = malloc(i + 1);
-	
+	j = 0;
+	while(j < i)
+	{
+		line[j] = (*str)[j];
+		j++;
+	}
+	line[j] = '\0';
+	return(line);
 }
 
 char *get_next_line(int fd)
